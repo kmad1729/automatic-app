dd63b7c9cad94ed454a559ac4bcb39c87786f338
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-03-02 11:47:38 -0800

    adding more features. can tweet the number of miles driver

--- automatic_app/urls.py
+++ automatic_app/urls.py
@@ -3,14 +3,16 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+#import hello.views
+import scoresheet_app.views
 
 # Examples:
 # url(r'^$', 'automatic_app.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^$', hello.views.index, name='index'),
+    #url(r'^db', hello.views.db, name='db'),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', scoresheet_app.views.home, name='home'),
 ]
---
+++ scoresheet_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ScoresheetAppConfig(AppConfig):
+    name = 'scoresheet_app'
