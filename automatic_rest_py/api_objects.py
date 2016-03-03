d7c15f092f89849e5720efcd2c2934193f103a95
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-03-03 00:44:50 -0800

    adding day summary. moved tweet to day summary

--- automatic_rest_py/api_objects.py
+++ automatic_rest_py/api_objects.py
@@ -1,4 +1,7 @@
-from .utils import convert_date_str_to_date_obj, convert_date_iso_ts_to_date_obj
+from .utils import convert_date_str_to_date_obj, format_float, \
+    convert_date_iso_ts_to_date_obj, duration_seconds_to_minutes, \
+    distance_metres_to_miles, volume_liters_to_gallons
+
 
 
 class Trip:
@@ -13,4 +16,17 @@ class Trip:
                 setattr(self, k, convert_date_iso_ts_to_date_obj(v))
             else:
                 setattr(self, k, v)
+
+    @property
+    def duration_min(self):
+        return format_float(duration_seconds_to_minutes(self.duration_s))
+
+    @property
+    def distance_mi(self):
+        return format_float(distance_metres_to_miles(self.distance_m))
+
+    @property
+    def fuel_volume_g(self):
+        return format_float(volume_liters_to_gallons(self.fuel_volume_l))
+
 
